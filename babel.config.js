a717a367b27c0f0cc66f1476406bb58e17b64b2e
Stephen da Conceicao <daconcei472@gmail.com>
2019-03-24 14:30:53 -0400

    Using env for api url

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,9 @@
 module.exports = api => {
     api.cache(false);
     return {
-        presets: ['module:metro-react-native-babel-preset'],
+        presets: [
+            "module:metro-react-native-babel-preset",
+            "module:react-native-dotenv"],
         plugins: [
             "@babel/plugin-proposal-export-default-from",
             "@babel/plugin-proposal-export-namespace-from"
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import {AppRegistry} from 'react-native';
+import {API_BASE_URL} from 'react-native-dotenv'
 import axios from 'axios';
 
 import {apiConfig} from 'cookbookery-shared';
@@ -6,6 +7,6 @@ import {apiConfig} from 'cookbookery-shared';
 import App from './src/App';
 import {name as appName} from './app.json';
 
-apiConfig(axios, "http://localhost:3001", false);
+apiConfig(axios, API_BASE_URL, false);
 
 AppRegistry.registerComponent(appName, () => App);
